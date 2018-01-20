55ce4c75e04bb1cdb8b0e65eab45d12f6a184e72
RoccoPic <rocco.piccirillo1@marist.edu>
2018-01-20 17:26:17 -0500

    Created folder hierarchy
    fixing folders

---
+++ prj/0/driver_proj0.java
@@ -0,0 +1,11 @@
+/* CMPT 220
+ * Rocco Piccirillo
+ * Project 0
+ * 1/20/18
+ */
+public class driver_proj0 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
+}
